f45bbbb227e0c74a33db86703f23106abcfe6eba
Moore <crimzero@hotmail.com>
2019-03-25 13:46:34 -0500

    need to add a cheese class

---
+++ src/main/java/org/launchcode/cheesemvc/controllers/CheeseController.java
@@ -0,0 +1,44 @@
+package org.launchcode.cheesemvc.controllers;
+
+import org.launchcode.cheesemvc.models.Cheese;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+    HashMap<String, String> cheeses = new HashMap<>();
+
+
+    @RequestMapping(value = "")
+    public String index(Model model)  {
+
+        model.addAttribute("cheeseList", cheeses);
+        model.addAttribute("title", "My Cheeses");
+        return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model) {
+        model.addAttribute("title", "Add Cheese");
+        return "cheese/add";
+    }
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName, @RequestParam String cheeseDesc) {
+        Cheese cheese = new Cheese();
+        HashMap<String, String> aCheese = new HashMap<>();
+        cheese.setName();
+
+        cheeses.put(cheeseName, cheeseDesc);
+
+        return "redirect:";
+    }
+}
---
+++ src/main/java/org/launchcode/cheesemvc/models/Cheese.java
@@ -0,0 +1,23 @@
+package org.launchcode.cheesemvc.models;
+
+public class Cheese {
+
+    public String name;
+    public String description;
+
+    public void setName() {
+        String name = this.name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setDesc() {
+        String description = this.description;
+    }
+
+    public String getDesc() {
+        return description;
+    }
+}
